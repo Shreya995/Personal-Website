274ef071cbc3533dc94d085509b5df0050a12fd7
Shreya995 <shreya.m0330@gmail.com>
2024-07-31 19:28:31 +0530

    Added auto-type effect

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function toggleMenu(){
         icon.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="4rem" width="4rem" xmlns="http://www.w3.org/2000/svg"><path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"></path></svg>'
     }
 }
+
+//Auto Typing Effect
+
+var typed = new Typed(".auto-type", {
+    strings: ["Software Developer", "Lifelong Learner", "Food Enthusiast"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
